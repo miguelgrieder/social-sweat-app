3ca0cad396d431b078d3427b36b3f4d970b2a9da
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-08-31 20:32:53 -0300

    feat: Adds more basic files and modals

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {Stack, useRouter} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import Colors from '@/constants/Colors';
+import {TouchableOpacity} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -47,10 +47,47 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
+  const router = useRouter()
   return (
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(modals)/login"
+          options={{
+            presentation: 'modal',
+            title: 'Log in or sign up',
+            headerTitleStyle: {
+              fontFamily: 'mon-sb',
+            },
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => router.back()}>
+                <Ionicons name="close-outline" size={28} />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: '' }} />
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: '#fff',
+                borderColor: Colors.grey,
+                borderRadius: 20,
+                borderWidth: 1,
+                padding: 4,
+              }}>
+              <Ionicons name="close-outline" size={22} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
       </Stack>
   );
 }

c19f471851ac43553064eaca33a21c9353e8e4c2
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-01 13:43:01 -0300

    feat: Start ExploreHeader and Listings components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
-import { Link } from 'expo-router';
+import { Stack } from 'expo-router';
+import Listings from '@/components/Listings';
+import ExploreHeader from '@/components/ExploreHeader';
 
 const Page = () => {
   return (
-    <View>
-      <Link href={'/(modals)/login'}>Login</Link>
-      <Link href={'/(modals)/booking'}>Booking</Link>
-      <Link href={'/listing/133'}>Listing details</Link>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader />,
+        }}
+      />
+      <Listings />
     </View>
   );
 };
---
+++ src/components/ExploreHeader.tsx
@@ -0,0 +1,135 @@
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+
+const categories = [
+  {
+    name: 'Tiny homes',
+    icon: 'home',
+  },
+  {
+    name: 'Cabins',
+    icon: 'house-siding',
+  },
+  {
+    name: 'Trending',
+    icon: 'local-fire-department',
+  },
+  {
+    name: 'Play',
+    icon: 'videogame-asset',
+  },
+  {
+    name: 'City',
+    icon: 'apartment',
+  },
+  {
+    name: 'Beachfront',
+    icon: 'beach-access',
+  },
+  {
+    name: 'Countryside',
+    icon: 'nature-people',
+  },
+];
+
+const ExploreHeader = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', paddingTop: 29 }}>
+      <View style={styles.container}>
+        <View style={styles.actionRow}>
+          <Link href={'/(modals)/booking'} asChild>
+            <TouchableOpacity>
+              <View style={styles.searchBtn}>
+                <Ionicons name="search" size={24} />
+                <View>
+                  <Text style={{ fontFamily: 'mon-sb' }}>Where to?</Text>
+                  <Text style={{ color: Colors.grey, fontFamily: 'mon' }}>Anywhere · Any week</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </Link>
+
+          <TouchableOpacity style={styles.filterBtn}>
+            <Ionicons name="options-outline" size={24} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: 130,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  actionRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingBottom: 16,
+  },
+
+  searchBtn: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    gap: 10,
+    padding: 14,
+    alignItems: 'center',
+    width: 280,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#c2c2c2',
+    borderRadius: 30,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.12,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  filterBtn: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#A2A0A2',
+    borderRadius: 24,
+  },
+  categoryText: {
+    fontSize: 14,
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    fontFamily: 'mon-sb',
+    color: '#000',
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 8,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 2,
+    paddingBottom: 8,
+  },
+});
+
+export default ExploreHeader;

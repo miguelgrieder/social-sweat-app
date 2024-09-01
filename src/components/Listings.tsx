dd10e7fc722e749b6def4a13f1cf20fba0ae5b1e
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-01 17:56:26 -0300

    feat: Implements listings image with description and animations

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -1,8 +1,17 @@
-import { View, Text, FlatList, ListRenderItem } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import React, { useEffect, useRef, useState } from 'react';
 import { defaultStyles } from '@/constants/Styles';
 import { Link } from 'expo-router';
-
+import { Listing } from '@/interfaces/listing';
+import { Ionicons } from '@expo/vector-icons';
+import Animated, { FadeInRight, FadeOutLeft } from 'react-native-reanimated';
 interface Props {
   listings: any[];
   category: string;
@@ -20,8 +29,30 @@ const Listings = ({ listings: items, category }: Props) => {
     }, 200);
   }, [category]);
 
-  const renderRow: ListRenderItem<any> = ({ item }) => (
-    <Link href={`/listing/${item.id}`}>Go there</Link>
+  const renderRow: ListRenderItem<Listing> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
+          <Animated.Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity style={{ position: 'absolute', right: 30, top: 30 }}>
+            <Ionicons name="heart-outline" size={24} color="#000" />
+          </TouchableOpacity>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 16, fontFamily: 'mon-sb' }}>{item.name}</Text>
+            <View style={{ flexDirection: 'row', gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: 'mon-sb' }}>{item.review_scores_rating / 20}</Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: 'mon' }}>{item.room_type}</Text>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+            <Text style={{ fontFamily: 'mon' }}>night</Text>
+          </View>
+        </Animated.View>
+      </TouchableOpacity>
+    </Link>
   );
   return (
     <View style={defaultStyles.container}>
@@ -29,5 +60,23 @@ const Listings = ({ listings: items, category }: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
 
 export default Listings;
---
+++ src/interfaces/listing.ts
@@ -0,0 +1,9 @@
+export interface Listing {
+  id: string;
+  name: string;
+  description: string;
+  medium_url: string;
+  review_scores_rating: string;
+  room_type: string;
+  price: string;
+}

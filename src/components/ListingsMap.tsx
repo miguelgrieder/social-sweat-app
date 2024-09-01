16b44099b44e2703a3082b97f2cc9d86ccce7b6d
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-01 20:17:19 -0300

    feat: Adds markers to the map, routing to each rental

--- src/components/ListingsMap.tsx
+++ src/components/ListingsMap.tsx
@@ -2,16 +2,22 @@ import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { defaultStyles } from '@/constants/Styles';
-
+import { Marker } from 'react-native-maps';
+import { ListingGeo } from '@/interfaces/listingGeo';
+import { router } from 'expo-router';
 interface Props {
   listings: any;
 }
 
 const INITIAL_REGION = {
-  latitude: 37.33,
-  longitude: -122,
+  latitude: 52.52,
+  longitude: 13.405,
   latitudeDelta: 9,
   longitudeDelta: 9,
+};
+
+const onMarkerSelected = (event: any) => {
+  router.push(`/listing/${event.properties.id}`);
 };
 
 const ListingsMap = ({ listings }: Props) => {
@@ -23,7 +29,22 @@ const ListingsMap = ({ listings }: Props) => {
         showsUserLocation
         showsMyLocationButton
         initialRegion={INITIAL_REGION}
-      />
+      >
+        {listings.features.map((item: ListingGeo) => (
+          <Marker
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+            key={item.properties.id}
+            onPress={() => onMarkerSelected(item)}
+          >
+            <View style={styles.marker}>
+              <Text style={styles.markerText}>€ {item.properties.price}</Text>
+            </View>
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
 };
@@ -31,6 +52,25 @@ const ListingsMap = ({ listings }: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  marker: {
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    elevation: 5,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: 'mon-sb',
   },
 });
 export default ListingsMap;

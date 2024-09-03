7f180576ef035c787800b63e8e139b660b70d440
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-02 23:25:26 -0300

    feat: Adds more basic elements to profile page

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,22 +1,40 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
 import { useAuth } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { defaultStyles } from '@/constants/Styles';
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
 
   return (
-    <View>
-      <Button title="Log out" onPress={() => signOut()} />
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name="notifications-outline" size={26} />
+      </View>
 
+      <Button title="Log out" onPress={() => signOut()} />
       {!isSignedIn && (
         <Link href={'/(modals)/login'}>
           <Text>Login</Text>
         </Link>
       )}
-    </View>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 24,
+  },
+  header: {
+    fontFamily: 'mon-b',
+    fontSize: 24,
+  },
+});
 
 export default Page;

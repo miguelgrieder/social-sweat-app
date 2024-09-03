a9266d6405cbdeb4aace91f7e5e5637e0277f791
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-02 23:45:39 -0300

    feat: Adds needed imports and variables for user info and editing

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -7,8 +7,8 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import React from 'react';
-import { useAuth } from '@clerk/clerk-expo';
+import React, { useEffect, useState } from 'react';
+import { useAuth, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '@/constants/Colors';
@@ -16,6 +16,23 @@ import { defaultStyles } from '@/constants/Styles';
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEmail(user.emailAddresses[0].emailAddress);
+  }, [user]);
+  const onSaveUser = async () => {};
+  const onCaptureImage = async () => {};
 
   return (
     <SafeAreaView style={defaultStyles.container}>
@@ -75,6 +92,13 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 50,
     backgroundColor: Colors.grey,
+  },
+  editRow: {
+    // flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
   },
 });
 

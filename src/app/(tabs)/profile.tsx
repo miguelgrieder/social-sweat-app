6d39c0a7fa11d7c8b76e8cdac817c814f85796de
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-02 23:31:18 -0300

    feat: Adds user image to profile if authenticated

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,4 +1,12 @@
-import { View, Text, Button, SafeAreaView, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import React from 'react';
 import { useAuth } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
@@ -15,6 +23,14 @@ const Page = () => {
         <Text style={styles.header}>Profile</Text>
         <Ionicons name="notifications-outline" size={26} />
       </View>
+
+      {user && (
+        <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </TouchableOpacity>
+        </View>
+      )}
 
       {isSignedIn && <Button title="Log Out" onPress={() => signOut()} color={Colors.dark} />}
       {!isSignedIn && (
@@ -35,6 +51,30 @@ const styles = StyleSheet.create({
   header: {
     fontFamily: 'mon-b',
     fontSize: 24,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: 'center',
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
   },
 });
 

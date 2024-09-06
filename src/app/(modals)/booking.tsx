1cfd7be5b7e949bdf150e36a94dd045601c20d0f
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-05 22:36:12 -0300

    feat: Implements the footer to booking modal

--- src/app/(modals)/booking.tsx
+++ src/app/(modals)/booking.tsx
@@ -1,11 +1,52 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { BlurView } from 'expo-blur';
+import { defaultStyles } from '@/constants/Styles';
+import Animated, { SlideInDown } from 'react-native-reanimated';
+import { router } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
+  const onClearAll = () => {};
+
   return (
     <BlurView intensity={90} style={styles.container} tint="light">
       <Text>BOOKING</Text>
+
+      {/* Footer */}
+      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+        <View
+          style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}
+        >
+          <TouchableOpacity
+            style={{ height: '100%', justifyContent: 'center' }}
+            onPress={onClearAll}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontFamily: 'mon-sb',
+                textDecorationLine: 'underline',
+              }}
+            >
+              Clear all
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[defaultStyles.btn, { paddingRight: 20, paddingLeft: 50 }]}
+            onPress={() => router.back()}
+          >
+            <Ionicons
+              name="search-outline"
+              size={24}
+              style={defaultStyles.btnIcon}
+              color={'#fff'}
+            />
+            <Text style={defaultStyles.btnText}>Search</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </BlurView>
   );
 };

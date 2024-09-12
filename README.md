d2083a2be995e5f183b4004974ca6735a4c4fe25
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-11 22:13:35 -0300

    feat: Update configs and settings

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 
 Tutorial: https://www.youtube.com/watch?v=iWzUZiVoiR0
+
+Packages
+npm install @clerk/clerk-expo
+npx expo install react-native-modern-datepicker
+npx expo install expo-blur
+npx expo install react-native-maps
+npx expo install react-native-gesture-handler
+npm i @gorhom/bottom-sheet@^4
+npx expo install expo-image-picker
+npx expo install expo-haptics
+npm install expo-router@~3.5.20
+npx expo install react-native-map-clustering
+npx expo install expo-secure-store
+
+
+npx expo install expo-auth-session expo-crypto
+npx expo install expo-web-browser
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
-module.exports = function (api) {
+/** @type {import('@babel/core').TransformOptions['plugins']} */
+const plugins = [
+  "@babel/plugin-proposal-export-namespace-from",
+  /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
+  "react-native-reanimated/plugin",
+]
+
+/** @type {import('@babel/core').TransformOptions} */
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {},
+    },
+    plugins,
   };
 };

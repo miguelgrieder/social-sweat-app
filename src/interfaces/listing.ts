559025dcf6d7ee3ad8dbe3efb3e673234e878101
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-15 18:51:15 -0300

    refactor: Updates interface with inner interfaces

--- src/interfaces/listing.ts
+++ src/interfaces/listing.ts
@@ -4,41 +4,57 @@ export interface Listing {
   description: string;
   activity_type: string;
   sport_type: string;
-  price: {
-    value: number;
-    unit: string;
-  };
-  location: {
-    country: string;
-    area: string;
-    city: string;
-    smart_location: string;
-    geometry: {
-      type: string;
-      coordinates: {
-        latitude: number;
-        longitude: number;
-      };
-    };
-  };
-  participants: {
-    current: number;
-    max: number | null;
-  };
-  reviews: {
-    number_of_reviews: number;
-    review_scores_rating: number;
-  };
+  price: Price;
+  location: Location;
+  participants: Participants;
+  reviews: Reviews;
   pictures: string[]; // Array of URLs
-  host: {
-    host_picture_url: string;
-    host_name: string;
-    host_since: string;
-  };
-  datetimes: {
-    datetime_created: string;
-    datetime_deleted: string | null;
-    datetime_start: string;
-    datetime_finish: string;
-  };
+  host: Host;
+  datetimes: DateTimes;
+}
+
+interface Price {
+  value: number;
+  unit: string;
+}
+
+interface Location {
+  country: string;
+  area: string;
+  city: string;
+  smart_location: string;
+  geometry: Geometry;
+}
+
+interface Geometry {
+  type: string;
+  coordinates: Coordinates;
+}
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface Participants {
+  current: number;
+  max: number | null;
+}
+
+interface Reviews {
+  number_of_reviews: number;
+  review_scores_rating: number;
+}
+
+interface Host {
+  host_picture_url: string;
+  host_name: string;
+  host_since: string;
+}
+
+interface DateTimes {
+  datetime_created: string;
+  datetime_deleted: string | null;
+  datetime_start: string;
+  datetime_finish: string;
 }

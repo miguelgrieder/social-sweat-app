dcf3add9f66b372adfffb5db2f25e5cf14a2c09f
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-16 22:28:51 -0300

    feat: Use useFocusEffect to always load activities when entering

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Stack } from 'expo-router';
 import ExploreHeader from '@/components/ExploreHeader';
 import ListingsMap from '@/components/ListingsMap';
@@ -6,21 +6,25 @@ import ListingsBottomSheet from '@/components/ListingsBottomSheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Screen } from 'src/components/Screen';
 import { fetchActivities } from '@/api/filter_activities';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Page = () => {
   const [category, setCategory] = useState<string>('Trending');
   const [items, setItems] = useState<any[]>([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const filterBody = {
-        activity_id: null,
+  useFocusEffect( // Always load activities when enter the page
+    useCallback(() => {
+      const getData = async () => {
+        const filterBody = {
+          activity_id: null,
+        };
+        const activities = await fetchActivities(filterBody);
+        setItems(activities);
       };
-      const activities = await fetchActivities(filterBody);
-      setItems(activities);
-    };
-    getData();
-  }, []);
+
+      getData();
+    }, []) // Empty dependency array ensures the callback doesn't change
+  );
 
   const onDataChanged = (category: string) => {
     setCategory(category);

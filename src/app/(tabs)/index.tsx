c662ecdaf4ae206dea70f85102975d9c6a858462
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-09-19 22:50:52 -0300

    feat: (index.tsx) Uses TouchableOpacity for activity_creation button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Button, ViewStyle } from 'react-native';
+import { Text, TouchableOpacity, ViewStyle } from 'react-native';
 import React from 'react';
 import { Screen } from 'src/components/Screen';
 import { translate } from '@/app/services/translate';
 import { Link } from 'expo-router';
-import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { spacing } from '@/constants/spacing';
 
 const Home = () => {
   return (
     <Screen preset="fixed" contentContainerStyle={$container} safeAreaEdges={['top']}>
       <Link href={'/(modals)/activity_creation'} asChild>
-        <Button title={'activity_creation.tsx'} color={Colors.dark} />
+        <TouchableOpacity style={defaultStyles.btn}>
+          <Text style={defaultStyles.btnText}>{translate('create_activity_screen.title')}</Text>
+        </TouchableOpacity>
       </Link>
     </Screen>
   );
@@ -19,4 +22,5 @@ export default Home;
 
 const $container: ViewStyle = {
   flex: 1,
+  padding: spacing.md,
 };

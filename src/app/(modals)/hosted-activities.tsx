3523eb156a5e5927982bc07e82cc410f1fa7a6f1
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-06 16:13:28 -0300

    fix: Resets FilterActivityInputContext filters when opened NextActivitiesTab and HostedActivitiesTab

--- src/app/(modals)/hosted-activities.tsx
+++ src/app/(modals)/hosted-activities.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ActivitiesPage from '@/components/ActivitiesPage';
 import { useUser } from '@clerk/clerk-expo';
+import { useFilters } from '@/context/FilterActivityInputContext';
 
 export default function HostedActivitiesTab() {
   const { user } = useUser();
+  const { setFilters } = useFilters();
+
+  useEffect(() => {
+    setFilters({});
+  }, [setFilters]);
 
   const initialFilter = {
     host_user_id: user.id,

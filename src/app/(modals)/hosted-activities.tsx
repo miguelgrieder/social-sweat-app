324f7314ea33679753e3076c2ec7f674d955dff9
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-08 23:58:34 -0300

    feat: (hosted-activities.tsx, locales) Returns NotLoggedInMessage from HostedActivitiesTab if not authenticated

--- src/app/(modals)/hosted-activities.tsx
+++ src/app/(modals)/hosted-activities.tsx
@@ -2,14 +2,37 @@ import React, { useEffect } from 'react';
 import ActivitiesPage from '@/components/ActivitiesPage';
 import { useUser } from '@clerk/clerk-expo';
 import { useFilters } from '@/context/FilterActivityInputContext';
+import NotLoggedInMessage from '@/components/NotLoggedInMessage';
+import { Stack } from 'expo-router'; // Import Stack for screen options
+import { translate } from '@/app/services/translate';
 
 export default function HostedActivitiesTab() {
-  const { user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
   const { setFilters } = useFilters();
 
   useEffect(() => {
     setFilters({});
   }, [setFilters]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  if (!isSignedIn || !user) {
+    return (
+      <>
+        <Stack.Screen
+          options={{
+            headerShown: true,
+            title: translate('common.unauthorized'),
+            headerShadowVisible: false,
+            headerTitleAlign: 'center',
+          }}
+        />
+        <NotLoggedInMessage addLink={false} />
+      </>
+    );
+  }
 
   const initialFilter = {
     host_user_id: user.id,

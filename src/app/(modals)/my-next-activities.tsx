b51ac18d6ea9465e54f2cf00bebbd2b824136988
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-09 00:02:17 -0300

    feat: (my-next-activities.tsx) Returns NotLoggedInMessage from NextActivitiesTab if not authenticated

--- src/app/(modals)/my-next-activities.tsx
+++ src/app/(modals)/my-next-activities.tsx
@@ -2,14 +2,37 @@ import React, { useEffect } from 'react';
 import ActivitiesPage from '@/components/ActivitiesPage';
 import { useUser } from '@clerk/clerk-expo';
 import { useFilters } from '@/context/FilterActivityInputContext';
+import NotLoggedInMessage from '@/components/NotLoggedInMessage';
+import { Stack } from 'expo-router';
+import { translate } from '@/app/services/translate';
 
 export default function NextActivitiesTab() {
-  const { user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
   const { setFilters } = useFilters();
 
   useEffect(() => {
     setFilters({});
   }, [setFilters]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  if (!isSignedIn || !user) {
+    return (
+      <>
+        <Stack.Screen
+          options={{
+            headerShown: true,
+            title: translate('common.unauthorized'),
+            headerShadowVisible: false,
+            headerTitleAlign: 'center',
+          }}
+        />
+        <NotLoggedInMessage addLink={false} />
+      </>
+    );
+  }
 
   const initialFilter = {
     participant_user_id: user.id,

fb5656dc3b3652ea5421bacb6321431a8b47e2e2
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-15 22:34:15 -0300

    data: Adds app.config.js, eas.json file

--- app.config.js
+++ app.config.js
@@ -1,10 +1,17 @@
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      apiMicroserviceUrl: process.env.EXPO_PUBLIC_API_MICROSERVICE,
-      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
-      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+export default ({ config }) => ({
+  ...config,
+  extra: {
+    eas: {
+      projectId: "2861e101-a356-495c-b3f1-382c44216a5a",
     },
-  };
-};
+    apiMicroserviceUrl: process.env.EXPO_PUBLIC_API_MICROSERVICE,
+    clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+  updates: {
+    url: "https://u.expo.dev/2861e101-a356-495c-b3f1-382c44216a5a",
+  },
+  runtimeVersion: {
+    policy: "appVersion", // Recommended runtime versioning policy
+  }
+});

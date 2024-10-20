b4ce82e42f6838c6ad3729c83cd5efd787d36583
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-20 00:04:33 -0300

    feat: Improves HorizontalCards.tsx with a better look (color, icon, structure)

--- src/components/user/HorizontalCards.tsx
+++ src/components/user/HorizontalCards.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { translate } from '@/app/services/translate';
 import { spacing } from '@/constants/spacing';
-import Colors from '@/constants/Colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 interface UserMetrics {
   activities_participated?: number;
@@ -17,17 +17,32 @@ interface HorizontalCardsProps {
 const HorizontalCards: React.FC<HorizontalCardsProps> = ({ userMetrics }) => {
   return (
     <View style={styles.cardsContainer}>
-      <View style={styles.card}>
+      <View style={[styles.card, styles.participatedCard]}>
         <Text style={styles.cardTitle}>{translate('profile_screen.activities_participated')}</Text>
-        <Text style={styles.cardValue}>{userMetrics?.activities_participated || 0}</Text>
+        <View style={styles.bottomContainer}>
+          <View style={styles.iconBackground}>
+            <MaterialCommunityIcons name="check-outline" size={20} color="#fff" />
+          </View>
+          <Text style={styles.cardValue}>{userMetrics?.activities_participated || 0}</Text>
+        </View>
       </View>
-      <View style={styles.card}>
+      <View style={[styles.card, styles.participatingCard]}>
         <Text style={styles.cardTitle}>{translate('profile_screen.activities_participating')}</Text>
-        <Text style={styles.cardValue}>{userMetrics?.activities_participating || 0}</Text>
+        <View style={styles.bottomContainer}>
+          <View style={styles.iconBackground}>
+            <MaterialCommunityIcons name="run-fast" size={20} color="#fff" />
+          </View>
+          <Text style={styles.cardValue}>{userMetrics?.activities_participating || 0}</Text>
+        </View>
       </View>
-      <View style={styles.card}>
+      <View style={[styles.card, styles.createdCard]}>
         <Text style={styles.cardTitle}>{translate('profile_screen.activities_created')}</Text>
-        <Text style={styles.cardValue}>{userMetrics?.activities_created || 0}</Text>
+        <View style={styles.bottomContainer}>
+          <View style={styles.iconBackground}>
+            <MaterialCommunityIcons name="creation" size={20} color="#fff" />
+          </View>
+          <Text style={styles.cardValue}>{userMetrics?.activities_created || 0}</Text>
+        </View>
       </View>
     </View>
   );
@@ -44,29 +59,49 @@ const styles = StyleSheet.create({
   },
   card: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderRadius: spacing.md,
+    borderRadius: spacing.sm,
     marginHorizontal: spacing.xxs,
-    alignItems: 'center',
-    elevation: 2,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowRadius: 6,
-    shadowOffset: {
-      width: 1,
-      height: 2,
-    },
-    padding: spacing.sm,
+    paddingVertical: spacing.xs,
+    paddingHorizontal: spacing.sm,
+    elevation: spacing.xxs,
+    position: 'relative',
+    justifyContent: 'space-between',
+    height: 100,
+  },
+  participatedCard: {
+    backgroundColor: '#FF6B6B',
+  },
+  participatingCard: {
+    backgroundColor: '#1DD1A1',
+  },
+  createdCard: {
+    backgroundColor: '#576574',
   },
   cardTitle: {
     fontSize: 14,
-    color: '#666',
-    marginBottom: spacing.sm,
-    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    color: '#fff',
+    textAlign: 'left',
+    marginBottom: spacing.xxs,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  iconBackground: {
+    width: 30,
+    height: 30,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    borderRadius: spacing.md,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   cardValue: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: Colors.primary,
+    color: '#fff',
+    marginRight: spacing.xs,
   },
 });

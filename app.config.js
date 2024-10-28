bb5f64e175a4521266e86e27329f87b66817a70a
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-10-28 00:21:33 -0300

    build: Adds the new expo project id

--- app.config.js
+++ app.config.js
@@ -2,14 +2,14 @@ export default ({ config }) => ({
   ...config,
   extra: {
     eas: {
-      projectId: "2861e101-a356-495c-b3f1-382c44216a5a",
+      projectId: "1937b145-840a-46f9-9132-a4d3fe42925f",
     },
     apiMicroserviceUrl: process.env.EXPO_PUBLIC_API_MICROSERVICE,
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
     googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
   updates: {
-    url: "https://u.expo.dev/2861e101-a356-495c-b3f1-382c44216a5a",
+    url: "https://u.expo.dev/1937b145-840a-46f9-9132-a4d3fe42925f",
   },
   runtimeVersion: {
     policy: "appVersion", // Recommended runtime versioning policy
